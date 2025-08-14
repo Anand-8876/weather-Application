46474ba6673f883fd5bfeb14401f94c46c4e1dd2
Anand-8876 <anandmannusingh@gmail.com>
2025-08-14 21:58:24 +0530

    change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // ✅ Allows production builds to complete even with ESLint errors
+    
     ignoreDuringBuilds: true,
   },
 };
